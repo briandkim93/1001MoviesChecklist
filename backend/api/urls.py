94dd88dcd310febc9e68b4fd70793bafeab670db
Brian Kim <briandkim93@gmail.com>
2018-08-23 14:14:48 -0700

    Implement token-based account authentication

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import AccountViewSet
+from .views import AccountViewSet, LoginViewSet
 
 router = DefaultRouter()
 router.register('account', AccountViewSet)
+router.register('login', LoginViewSet, base_name='login')
 
 urlpatterns = [
     path('', include(router.urls))
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from .serializers import AccountSerializer
 from .models import Account
@@ -7,3 +9,9 @@ from .models import Account
 class AccountViewSet(ModelViewSet):
     serializer_class = AccountSerializer
     queryset = Account.objects.all()
+
+class LoginViewSet(ViewSet):
+    serializer_class = AuthTokenSerializer
+
+    def create(self, request):
+        return ObtainAuthToken().post(request)

b2a8a6575e06b9d42792663aff664a4aac1ddcfc
Brian Kim <briandkim93@gmail.com>
2018-08-25 12:14:08 -0700

    Add and configure redux, react-redux, redux-promise, and redux-persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+
+import reducers from './reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: []
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = createStore(persistedReducer, applyMiddleware(ReduxPromise));
+const persistor = persistStore(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>, 
+  document.getElementById('root')
+);

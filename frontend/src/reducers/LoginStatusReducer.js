48737827ac8b1541b8ae173556733cf664d8785f
Brian Kim <briandkim93@gmail.com>
2018-08-29 17:12:51 -0700

    Redefine initial state

--- frontend/src/reducers/LoginStatusReducer.js
+++ frontend/src/reducers/LoginStatusReducer.js
@@ -1,6 +1,6 @@
 import ACTION_TYPES from '../actions/types';
 
-function LoginStatusReducer(state=1, action) {
+function LoginStatusReducer(state={}, action) {
   switch (action.type) {
     case ACTION_TYPES.LOGIN:
       return action.payload;

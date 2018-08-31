df10a09cd673faa2be91f5ce6702863348cc0b43
Brian Kim <briandkim93@gmail.com>
2018-08-30 22:00:05 -0700

    Remove numbers only password validator

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,9 +79,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
 ]
 
 LANGUAGE_CODE = 'en-us'

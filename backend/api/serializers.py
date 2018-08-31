d0a5ade5a2ef8eafe86582ad6c0f5a83af8b5c50
Brian Kim <briandkim93@gmail.com>
2018-08-30 21:24:50 -0700

    Seperate password validation into viewset function

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.core import exceptions
 from django.contrib.auth.password_validation import validate_password
+from django.utils.translation import gettext as _
 from rest_framework.serializers import ModelSerializer, ValidationError
 
 from .models import Account, Movie
@@ -13,23 +14,19 @@ class AccountSerializer(ModelSerializer):
 
     def create(self, validated_data):
         account = Account(username=validated_data['username'], email=validated_data['email'])
-        try:
-            validate_password(password=validated_data['password'])
-        except exceptions.ValidationError as e:
-            raise ValidationError({'password': eval(str(e))})
         account.set_password(validated_data['password'])
         account.save()
         return account
 
     def update(self, instance, validated_data):
         instance.email = validated_data['email']
-        try:
-            validate_password(password=validated_data['password'])
-        except exceptions.ValidationError as e:
-            raise ValidationError({'password': eval(str(e))})
         instance.set_password(validated_data['password'])
         instance.save()
         return instance
+
+    def validate_password(self, value):
+        validate_password(password=value)
+        return value
 
 class MovieSerializer(ModelSerializer):
     class Meta:

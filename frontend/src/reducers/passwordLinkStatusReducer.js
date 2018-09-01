f3ae8e28002fc0d6365e034d39ac516eed9af6b3
Brian Kim <briandkim93@gmail.com>
2018-08-31 18:55:05 -0700

    Fix filename capitalization

--- frontend/src/reducers/passwordLinkStatusReducer.js
+++ frontend/src/reducers/passwordLinkStatusReducer.js
@@ -1,6 +1,6 @@
 import ACTION_TYPES from '../actions/types';
 
-function passwordLinkStatusReducer(state={}, action) {
+function PasswordLinkStatusReducer(state={}, action) {
   switch (action.type) {
     case ACTION_TYPES.SEND_RESET_PASSWORD_LINK:
       return action.payload;
@@ -9,4 +9,4 @@ function passwordLinkStatusReducer(state={}, action) {
   }
 }
 
-export default passwordLinkStatusReducer;
+export default PasswordLinkStatusReducer;

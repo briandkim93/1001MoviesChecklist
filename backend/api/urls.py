9ce129025afda73881573345ae346edb8bf8246c
Brian Kim <briandkim93@gmail.com>
2018-09-16 03:03:45 -0700

    Merge api roots

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,26 @@
 from django.urls import include, path
 
-from rest_framework.routers import DefaultRouter
+from knox import views as knox_views
+from rest_framework_social_oauth2 import views as rest_framework_social_oauth2_views
 
-from .views import AccountViewSet, MovieViewSet
-
-router = DefaultRouter()
-router.register('account', AccountViewSet)
-router.register('movie', MovieViewSet)
+from .views import AccountListView, AccountDetailView, MovieListView, MovieDetailView, EmailVerifyView, EmailVerifyConfirmView, PasswordResetView, PasswordResetConfirmView, LoginView, ConvertTokenFBView
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('api/movie/', MovieListView.as_view(), name='movie_list'),
+    path('api/movie/<int:pk>/', MovieDetailView.as_view(), name='movie_detail'),
+
+    path('api/auth/account/', AccountListView.as_view(), name='account_list'),
+    path('api/auth/account/<int:pk>/', AccountDetailView.as_view(), name='account_detail'),
+
+    path('api/auth/email/verify/', EmailVerifyView.as_view(), name='email_verify'),
+    path('api/auth/email/verify/confirm/', EmailVerifyConfirmView.as_view(), name='email_verify_confirm'),
+    
+    path('api/auth/password/reset/', PasswordResetView.as_view(), name='password_reset'),
+    path('api/auth/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    path('api/auth/login/', LoginView.as_view(), name='knox_login'),
+    path('api/auth/logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
+    
+    path('api/auth/social/convert-token/', ConvertTokenFBView.as_view(), name='convert_token'),
+    path('api/auth/social/revoke-token/', rest_framework_social_oauth2_views.RevokeTokenView.as_view(), name='revoke_token'),
 ]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,37 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from knox import views as knox_views
 from oauth2_provider import views as oauth2_provider_views
-from rest_framework_social_oauth2 import views as social_oauth2_views
 
 from .confidential import ADMIN_URL
 from .views import api_root
-from api.views import EmailVerifyView, EmailVerifyConfirmView, PasswordResetView, PasswordResetConfirmView, LoginView, ConvertTokenFBView
-
-social_urlpatterns = [
-    path('', include('social_django.urls')),
-    path('', include('rest_framework_social_oauth2.urls'))
-]
 
 urlpatterns = [
-    path('api/', include('api.urls')),
-
-    path('api/auth/', api_root),
-
-    path('api/auth/email/verify/', EmailVerifyView.as_view(), name='email_verify'),
-    path('api/auth/email/verify/confirm/', EmailVerifyConfirmView.as_view(), name='email_verify_confirm'),
+    path('', include(('api.urls', 'api'), namespace='api')),
     
-    path('api/auth/password/reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('api/auth/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('api/', api_root),
 
-    path('api/auth/login/', LoginView.as_view(), name='knox_login'),
-    path('api/auth/logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
-    
-    path('', include('social_django.urls', namespace="social")),
-    path('api/auth/social/authorize/', oauth2_provider_views.AuthorizationView.as_view(), name="authorize"),
-    path('api/auth/social/convert-token/', ConvertTokenFBView.as_view(), name="convert_token"),
-    path('api/auth/social/revoke-token/', social_oauth2_views.RevokeTokenView.as_view(), name="revoke_token"),
+    path('', include('social_django.urls', namespace='social')),
+    path('api/auth/social/authorize/', oauth2_provider_views.AuthorizationView.as_view(), name='authorize'),
 
     path(ADMIN_URL, admin.site.urls)
 ]

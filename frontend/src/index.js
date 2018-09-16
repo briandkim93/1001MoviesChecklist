17b5b7ca0d6d2d2cd37b7f5b824a149785c0401c
Brian Kim <briandkim93@gmail.com>
2018-09-16 03:06:20 -0700

    Fix non-persisting user_info state

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 
 import ReduxPromise from 'redux-promise';
 
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage'
 import { persistStore, persistReducer } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -18,10 +19,11 @@ import reducers from './reducers';
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: autoMergeLevel2,
   blacklist: [
     'displaySignup',
     'displayLogin',
-    'displayReset',
+    'displayPasswordResetRequest',
     'signupStatus',
     'loginStatus',
     'facebookLoginStatus',

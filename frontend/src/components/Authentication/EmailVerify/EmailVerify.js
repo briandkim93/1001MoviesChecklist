4ea6477a2ea0d84408e12f252f061e5eb4b10f91
Brian Kim <briandkim93@gmail.com>
2018-09-27 11:15:46 -0700

    Fix unexpected wrapping on homepage link

--- frontend/src/components/Authentication/EmailVerify/EmailVerify.js
+++ frontend/src/components/Authentication/EmailVerify/EmailVerify.js
@@ -75,63 +75,67 @@ class EmailVerify extends Component {
   
   render() {
     return (
-      <div className="row justify-content-center mt-3">
-        <form 
-          className="account-settings-form col-11 center-block text-white-50 p-3" 
-          encType='multipart/form-data' 
-          onSubmit={this.handleFormSubmit}
-        >
-          <div className="text-center">
-            <h1 className="font-weight-light text-warning mb-1">
-              Verify Email
-            </h1>
+      <div>
+        <div className="row justify-content-center mt-3">
+          <form 
+            className="account-settings-form col-11 center-block text-white-50 p-3" 
+            encType='multipart/form-data' 
+            onSubmit={this.handleFormSubmit}
+          >
+            <div className="text-center">
+              <h1 className="font-weight-light text-warning mb-1">
+                Verify Email
+              </h1>
+            </div>
+            <hr />
+            {this.state.response.status === 1
+              ? (
+                <div className="text-center">
+                  <div className="my-2">
+                    {this.state.response.message}
+                  </div>
+                  <div className="mb-5">
+                    If you are not logged in, please login to continue.
+                  </div>
+                </div>
+              )
+              : (
+                <div>
+                  <div className="form-group">
+                    <label htmlFor="verify-username">Username:</label>
+                    <input 
+                      id="verify-username" 
+                      className="account-settings-input form-control border-black" 
+                      type="text" 
+                      value={this.state.username} 
+                      onChange={this.handleInputChange} 
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="verify-password">Password:</label>
+                    <input 
+                      id="verify-password"  
+                      className="account-settings-input form-control border-black" 
+                      type="password"
+                      value={this.state.password} 
+                      onChange={this.handleInputChange} 
+                    />
+                  </div>
+                  <div className="text-danger small">
+                    {this.state.response.status === 0 && this.state.response.message}
+                  </div>
+                  <button className="btn btn-warning float-right" type="submit">
+                    Verify Email
+                  </button>
+                </div>
+              )
+            }
+          </form>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-11 center-block text-center">
+            <Link className="text-link text-warning" to='/'>Back to Homepage</Link>
           </div>
-          <hr />
-          {this.state.response.status === 1
-            ? (
-              <div className="text-center">
-                <div className="my-2">
-                  {this.state.response.message}
-                </div>
-                <div className="mb-5">
-                  If you are not logged in, please login to continue.
-                </div>
-              </div>
-            )
-            : (
-              <div>
-                <div className="form-group">
-                  <label htmlFor="verify-username">Username:</label>
-                  <input 
-                    id="verify-username" 
-                    className="account-settings-input form-control border-black" 
-                    type="text" 
-                    value={this.state.username} 
-                    onChange={this.handleInputChange} 
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="verify-password">Password:</label>
-                  <input 
-                    id="verify-password"  
-                    className="account-settings-input form-control border-black" 
-                    type="password"
-                    value={this.state.password} 
-                    onChange={this.handleInputChange} 
-                  />
-                </div>
-                <div className="text-danger small">
-                  {this.state.response.status === 0 && this.state.response.message}
-                </div>
-                <button className="btn btn-warning float-right" type="submit">
-                  Verify Email
-                </button>
-              </div>
-            )
-          }
-        </form>
-        <div>
-          <Link className="text-link text-warning" to='/'>Back to Homepage</Link>
         </div>
       </div>
     );
--- frontend/src/components/Authentication/PasswordReset/PasswordReset.js
+++ frontend/src/components/Authentication/PasswordReset/PasswordReset.js
@@ -125,63 +125,67 @@ class PasswordReset extends Component {
   
   render() {
     return (
-      <div className="row justify-content-center mt-3">
-        <form 
-          className="account-settings-form col-11 center-block text-white-50 p-3" 
-          encType='multipart/form-data' 
-          onSubmit={this.handleFormSubmit}
-        >
-          <div className="text-center">
-            <h1 className="font-weight-light text-warning mb-1">
-              Password Reset
-            </h1>
-          </div>
-          <hr className="border-black" />
-          {this.state.response.status === 1
-            ? (
-                <div className="text-center">
-                  <div className="my-2">
+      <div>
+        <div className="row justify-content-center mt-3">
+          <form 
+            className="account-settings-form col-11 center-block text-white-50 p-3" 
+            encType='multipart/form-data' 
+            onSubmit={this.handleFormSubmit}
+          >
+            <div className="text-center">
+              <h1 className="font-weight-light text-warning mb-1">
+                Password Reset
+              </h1>
+            </div>
+            <hr className="border-black" />
+            {this.state.response.status === 1
+              ? (
+                  <div className="text-center">
+                    <div className="my-2">
+                      {this.state.response.message}
+                    </div>
+                    <div className="mb-5">
+                      Please login to continue.
+                    </div>
+                  </div>
+              )
+              : (
+                <div>
+                  <div className="form-group">
+                    <label htmlFor="reset-password1">Password:</label>
+                    <input 
+                      id="reset-password1" 
+                      className="account-settings-input form-control border-black" 
+                      type="password" 
+                      value={this.state.password1} 
+                      onChange={this.handleInputChange} 
+                    />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="reset-password2">Confirm Password:</label>
+                    <input 
+                      id="reset-password2" 
+                      className="account-settings-input form-control border-black" 
+                      type="password" 
+                      value={this.state.password2} 
+                      onChange={this.handleInputChange} 
+                    />
+                  </div>
+                  <div className="text-danger small">
                     {this.state.response.message}
                   </div>
-                  <div className="mb-5">
-                    Please login to continue.
-                  </div>
+                  <button className="btn btn-warning float-right" type="submit">
+                    Confirm
+                  </button>
                 </div>
-            )
-            : (
-              <div>
-                <div className="form-group">
-                  <label htmlFor="reset-password1">Password:</label>
-                  <input 
-                    id="reset-password1" 
-                    className="account-settings-input form-control border-black" 
-                    type="password" 
-                    value={this.state.password1} 
-                    onChange={this.handleInputChange} 
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="reset-password2">Confirm Password:</label>
-                  <input 
-                    id="reset-password2" 
-                    className="account-settings-input form-control border-black" 
-                    type="password" 
-                    value={this.state.password2} 
-                    onChange={this.handleInputChange} 
-                  />
-                </div>
-                <div className="text-danger small">
-                  {this.state.response.message}
-                </div>
-                <button className="btn btn-warning float-right" type="submit">
-                  Confirm
-                </button>
-              </div>
-            )
-          }
-        </form>
-        <div>
-          <Link className="text-link text-warning" to='/'>Back to Homepage</Link>
+              )
+            }
+          </form>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-11 center-block text-center">
+            <Link className="text-link text-warning" to='/'>Back to Homepage</Link>
+          </div>
         </div>
       </div>
     );

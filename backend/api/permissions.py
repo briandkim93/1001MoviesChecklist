01631f7faafb6baf9b21ca7e5bac60687295ab6d
Brian Kim <briandkim93@gmail.com>
2018-10-13 10:39:11 -0700

    Remove permission name trailers

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -1,12 +1,12 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsAdminUserOrCreateOnlyPermission(BasePermission):
+class IsAdminUserOrCreateOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method == 'POST':
             return True
         return request.user.is_staff
 
-class IsCurrentUserPermission(BasePermission):
+class IsCurrentUser(BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user.id == obj.id
 
